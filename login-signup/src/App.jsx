5e99eccf175894febfbd8946544a86513eb60e3e
mdjnaim <21-45039-2@student.aiub.edu>
2026-02-25 12:16:20 +0600

    login-signup

---
+++ login-signup/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import LoginSignup from './loginsignup/loginsignup';
+
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App
