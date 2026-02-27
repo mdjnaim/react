afb3bc24df4a91851c54e3b34dac1cce9bdb3bee
mdjnaim <21-45039-2@student.aiub.edu>
2026-02-27 21:02:20 +0600

    css

--- src/assets/nasted_if.jsx
+++ src/assets/nasted_if.jsx
@@ -36,22 +36,27 @@ function Nastedif() {
 
     };
 
-    return (<div className="container">
-    <div className="input"> 
-        <input type="number" value={yourScore} placeholder="Enter your score" required onChange={(e) => setYourScore(e.target.value)}/>
+return (
+    <div className="container">
+        <div className="input"> 
+            <label>Your Score:</label>
+            <br />
+            <input type="text" value={yourScore} placeholder="Enter your score" required onChange={(e) => setYourScore(e.target.value)}/>
+        </div>
+        <div className="inputs">
+            <label>Friend's Score:</label>
+            <br />
+            <input type="text" value={friendScore} placeholder="Enter your friend score" required onChange={(e) => setFriendScore(e.target.value)}/>
+        </div>
+        <div className="btn">
+            <button onClick={checkResult}>Check</button>
+        </div>
+        <div className="reset_btn">
+            <button onClick={handleReset}>Reset</button>
+        </div>
+        <h3>{message}</h3>
     </div>
-    <div className="input">
-        <input type="number" value={friendScore} placeholder="Enter your friend score" required onChange={(e) => setFriendScore(e.target.value)}/>
-    </div>
-    <div className="btn">
-        <button onClick={checkResult}>Check</button>
-    </div>
-    <div className="reset_btn">
-        <button onClick={handleReset}>Reset</button>
-    </div>
-    <h3>{message}</h3>
-</div>
-    );
+);
 }
 
 export default Nastedif;
