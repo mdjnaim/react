569514a3db7786b082e631b7545abc315acaa954
mdjnaim <21-45039-2@student.aiub.edu>
2026-02-27 17:59:22 +0600

    nasted if

---
+++ src/assets/nasted_if.jsx
@@ -0,0 +1,57 @@
+import './nasted_if.css';
+import { useState } from 'react';
+
+function Nastedif() {
+    const [yourScore, setYourScore] = useState('');
+    const [friendScore, setFriendScore] = useState('');
+    const [message, setMessage] = useState('');
+
+    const checkResult = () => {
+        const you = Number(yourScore);
+        const friend = Number(friendScore);
+        
+        if (you >= 80) {
+            if (friend >= 80) {
+                setMessage("Go for a lunch with your friend.");
+            }
+            else if(friend >= 60) {
+                setMessage("Tell your friend : Good luck next time.")
+            }
+            else if(friend >= 40) {
+                setMessage("Keep your friend's message unseen.");
+            }
+            else {
+                setMessage("Block your friend.")  
+            }
+        }
+        else{
+            setMessage("Go to home and sleep and act sad.")
+        }
+    };
+
+    const handleReset = () => {
+        setYourScore('');
+        setFriendScore('');
+        setMessage('');
+
+    };
+
+    return (<div className="container">
+    <div className="input"> 
+        <input type="number" value={yourScore} placeholder="Enter your score" required onChange={(e) => setYourScore(e.target.value)}/>
+    </div>
+    <div className="input">
+        <input type="number" value={friendScore} placeholder="Enter your friend score" required onChange={(e) => setFriendScore(e.target.value)}/>
+    </div>
+    <div className="btn">
+        <button onClick={checkResult}>Check</button>
+    </div>
+    <div className="reset_btn">
+        <button onClick={handleReset}>Reset</button>
+    </div>
+    <h3>{message}</h3>
+</div>
+    );
+}
+
+export default Nastedif;
